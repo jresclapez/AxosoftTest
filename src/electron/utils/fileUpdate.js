c6b58425b655ca0f9878e124d29aec214b466c20
jresclapez <jr87elx@gmail.com>
2021-11-26 16:18:40 +0100

    fileUpdate.js code fix

--- src/electron/utils/fileUpdate.js
+++ src/electron/utils/fileUpdate.js
@@ -4,29 +4,29 @@ const fileExists = require('./fileExists');
 function fileUpdate(file, formatFunction) {
   // if file not exists,   new file its created
   if (!fileExists(file)) {
-    fs.open(file, 'wx', function (error, fd) {
+    fs.open(file, 'wx', function (error) {
       if (error) {
         throw error;
+      } else {
+        // function that reads data from file, formats data and write formatted data into file
+        fs.readFile(file, 'utf8', function readFileCallback(error, data) {
+          if (error) {
+            throw error;
+          } else {
+            fs.writeFile(
+              file,
+              formatFunction(data),
+              function writeFileCallback(error) {
+                if (error) {
+                  throw error;
+                }
+              }
+            );
+          }
+        });
       }
     });
   }
-
-  // function that reads data from file, formats data and write formatted data into file
-  fs.readFile(file, 'utf8', function readFileCallback(error, data) {
-    if (error) {
-      throw error;
-    } else {
-      fs.writeFile(
-        file,
-        formatFunction(data),
-        function writeFileCallback(error) {
-          if (error) {
-            throw error;
-          }
-        }
-      );
-    }
-  });
 }
 
 module.exports = fileUpdate;
