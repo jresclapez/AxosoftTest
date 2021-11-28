9f91e5711ef47cc08b8e0694fee3776a2b524f2a
jresclapez <jr87elx@gmail.com>
2021-11-28 20:54:49 +0100

    fileUpdate.js converted to asynchronous function

--- src/electron/utils/fileUpdate.js
+++ src/electron/utils/fileUpdate.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const fileExists = require('./fileExists');
-// if file not exists,   new file its created
-function fileUpdate(file, formatFunction) {
-  if (!fileExists(file)) {
-    fs.openSync(file, 'wx');
-  }
-  const data = fs.readFileSync(file, { encoding: 'utf8', flag: 'r' });
-  fs.writeFileSync(file, formatFunction(data));
-}
 
+// if file not exists,   new file its created
+async function fileUpdate(file, onRead) {
+  if (!fileExists(file)) {
+    await fs.open(file, 'wx');
+  }
+  const data = await fs.readFile(file, { encoding: 'utf8', flag: 'r' });
+  await fs.writeFile(file, onRead(data));
+}
 module.exports = fileUpdate;
