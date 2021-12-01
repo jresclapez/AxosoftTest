7402253ee91b386e6d62d5942653c0a0ac59eaa4
jresclapez <jesclap@gmail.com>
2021-12-01 11:29:38 +0100

    function name fixed (searchTweetsByUsername)

--- src/electron/services/searchTweets.js
+++ src/electron/services/searchTweets.js
@@ -1,12 +1,12 @@
 const httpSearchTweets = require('../http/httpSearchTweets');
 const updateLastSearches = require('./updateLastSearches');
-const SearchTweetsByUsername = require('./SearchTweetsByUsername');
+const searchTweetsByUsername = require('./SearchTweetsByUsername');
 const isTwitterUsername = require('../utils/isTwitterUsername');
 
 async function searchTweets(searchText) {
   await updateLastSearches(searchText);
   if (isTwitterUsername(searchText)) {
-    return await SearchTweetsByUsername(searchText);
+    return await searchTweetsByUsername(searchText);
   }
   return await httpSearchTweets(searchText);
 }
